ecc09519d6812d3ff53aba2941dc83f57fb4181b
Pank1-2 <52472073+Pank1-2@users.noreply.github.com>
2025-03-16 15:48:32 -0700

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,33 @@
+function animateAndRedirect(event) {
+    event.preventDefault();
+
+    var btn = document.getElementById('submitBtn');
+    btn.classList.add('animate-on-submit');
+
+    btn.addEventListener('animationend', function () {
+        btn.classList.remove('animate-on-submit');
+        btn.closest('form').submit();
+    }, { once: true });
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.body.classList.add('fade-in');
+});
+
+document.querySelectorAll('a.button').forEach(button => {
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        var destination = this.href;
+        document.body.classList.remove('fade-in');
+        setTimeout(function() {
+            window.location.href = destination;
+        }, 500);
+    });
+});
+
+
+window.onload = function() {
+    var slidoChatVideo = document.getElementById('slidoChatVideo');
+    slidoChatVideo.playbackRate = 0.75;
+  };
+  
